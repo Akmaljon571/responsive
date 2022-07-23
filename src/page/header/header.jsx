95206c467f6ffdc571d5c8677880304cb237a9c1
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-07-23 05:18:58 +0500

    finish: finish

--- src/page/header/header.jsx
+++ src/page/header/header.jsx
@@ -19,16 +19,16 @@ function Header() {
           <img src={logo} alt="dc" />
         </a>
         <div className="navbar_ota">
-          <a href="#Skills" onClick={linkActivbe} className={"navbar_link"} >
+          <a href="#Skills" className={"navbar_link"} >
             {language[0][lan].header}
           </a>
           <div className="navbar_otaLink">
-            <a onClick={linkActivbe} className={"navbar_link"} href="#project">
+            <a className={"navbar_link"} href="#project">
              {language[0][lan].loyiha}
             </a>
             <img src={project} alt="dsd" />
           </div>
-          <a onClick={linkActivbe} className={"navbar_link"} href="#Contactme">
+          <a className={"navbar_link"} href="#Contactme">
            {language[0][lan].contack}
           </a>
           <select value={lan} className={'navbar_select'} onChange={select}>
